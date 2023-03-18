fe6025cc157c2fc90d40b1cebfe9b292629c0b55
Vinicius Pinheiro <carraravinicius155@gmail.com>
2023-03-18 18:08:12 -0400

    adição de analise de dados da Atividade2

--- Atividade1/views.py
+++ Atividade1/views.py
@@ -1,34 +1,34 @@
 from django.shortcuts import render
 from .models import Pagamento
 
-
+#Pega os dados do banco
 def getPagamento(request):
     pagamento = Pagamento.objects.all()
     return render(request, 'Atividade1_Index.html', {'pagamentos': pagamento})
 
 
 def postPagamento(request):
+    #Verifica a request
     if (request.method == 'POST'):
         nome = request.POST.get('nome')
         quantidade_horas = request.POST.get('quantidade_horas')
         turno = request.POST.get('turno')
         categoria = request.POST.get('categoria')
 
+        #Verifica se as respostas são as dentro do esperado
         try:
             quantidade_horas = float(quantidade_horas)
             assert quantidade_horas >= 0
             assert categoria in ['Gerente', 'Operario']
             assert turno in ['Matutino', 'Vespetino', 'Noturno']
+            
+        #Se não estiver dentro do padrão é mandado erro o user para o template novamente
         except (ValueError, AssertionError):
             return render(request, 'form.html', {'error': 'Parâmetros inválidos'})
 
-        # valorTotal:float; valorHora:float
         valor = verificacaoPagamento(quantidade_horas, categoria, turno)
         valorTotal = valor[0]
         valorHora = valor[1]
-
-        # valorTotal = valor[0]
-        # valorHora = valor[1]
 
         Pagamento.objects.create(
             nome=nome,
@@ -41,10 +41,12 @@ def postPagamento(request):
 
         pagamento = Pagamento.objects.all()
         return render(request, 'Atividade1_index.html', {'pagamentos': pagamento, 'valo': valor})
+    
+    #Manda novamente para o template
     else:
         return render(request, 'form.html')
 
-
+#Define o valor total e o valor da hora
 def verificacaoPagamento(quantidade_horas, categoria, turno):
     if categoria == 'Gerente' and turno == 'Noturno':
         valorHora = (1320 - 10/100) * 1.0
@@ -61,5 +63,5 @@ def verificacaoPagamento(quantidade_horas, categoria, turno):
         valorTotal = valorHora * quantidade_horas
         return (valorTotal, valorHora)
 
-    # retorna uma tupla vazia caso nenhuma das condições acima seja satisfeita
+    #Retorna uma tupla vazia caso nenhuma das condições acima seja satisfeita
     return ()
--- Atividade2/views.py
+++ Atividade2/views.py
@@ -1,20 +1,25 @@
 from django.shortcuts import render, redirect
 from .models import Elevadores
 
+#Pega os dados do banco de dados, realiza analise dos dados e manda para o template 
 def getElevadores(request):
     elevadores = Elevadores.objects.all()
     respostas = analises(elevadores)
     
     return render(request, 'Atividade2_Index.html', {'respostas': respostas})
     
+#Salva os dados dentro do banco de dados
 def postElevadores(request):
     if (request.method == 'POST'):
         elevador = request.POST.get('elevador')
         turno = request.POST.get('turno')
 
+        #Verificação se os dados estão colocados corretos
         try:
             assert elevador in ['A', 'B', 'C']
             assert turno in ['Matutino', 'Vespetino', 'Noturno']
+            
+        #Não corretos envia erro e retorna para o mesmo template 
         except (ValueError, AssertionError):
             return render(request, 'form2.html', {'error': 'Parâmetros inválidos'})
 
@@ -25,22 +30,60 @@ def postElevadores(request):
         
         return redirect('Atividade2:getElevadores')
 
-            
+    
+    #Caso não seja post retorna o template
     else:
         return render(request, 'form2.html')
-    
+
+
+#Realiza o processo de analise dos dados
 def analises(elevadores):
     
     # Inicializa o dicionário que irá armazenar as análises
-    respostas = {'A': {'Matutino': 0, 'Vespetino': 0, 'Noturno': 0},
-                     'B': {'Matutino': 0, 'Vespetino': 0, 'Noturno': 0},
-                     'C': {'Matutino': 0, 'Vespetino': 0, 'Noturno': 0}}
+    respostas = {   'A': {'Matutino': 0, 'Vespetino': 0, 'Noturno': 0},
+                    'B': {'Matutino': 0, 'Vespetino': 0, 'Noturno': 0},
+                    'C': {'Matutino': 0, 'Vespetino': 0, 'Noturno': 0},                    
+                    }
+    
+    # Variáveis para as análises das questões c, d e e
+    elevador_mais_utilizado = None
+    elevador_mais_utilizado_total = 0
+    periodo_mais_utilizado = None
+    periodo_mais_utilizado_total = 0
+    periodo_total = 0
+    periodo_menos_utilizado = None
+    periodo_menos_utilizado_total = float('inf')
     
     # Faz a análise dos dados
     for elevador in elevadores:
         elevador_str = elevador.elevador
         turno_str = elevador.turno
         respostas[elevador_str][turno_str] += 1
+        
+        # Análise para a questão c
+        if respostas[elevador_str][turno_str] > elevador_mais_utilizado_total:
+            elevador_mais_utilizado_total = respostas[elevador_str][turno_str]
+            elevador_mais_utilizado = elevador_str + '-' + turno_str
+        
+        # Análise para as questões d e e
+        periodo_total += 1
+        periodo_mais_utilizado_total_atual = respostas[elevador_str][turno_str]
+        if periodo_mais_utilizado_total_atual > periodo_mais_utilizado_total:
+            periodo_mais_utilizado_total = periodo_mais_utilizado_total_atual
+            periodo_mais_utilizado = turno_str
+        if periodo_mais_utilizado_total_atual < periodo_menos_utilizado_total:
+            periodo_menos_utilizado_total = periodo_mais_utilizado_total_atual
+            periodo_menos_utilizado = turno_str
+    
+    # Calcula a diferença percentual para a questão e
+        diferenca_percentual = (((periodo_mais_utilizado_total/periodo_total) * 100)
+                                - ((periodo_menos_utilizado_total/periodo_total)* 100))
+
+    
+    # Adiciona as análises das questões c, d e e ao dicionário respostas
+    respostas['Elevador_mais_utilizado'] = elevador_mais_utilizado
+    respostas['Periodo _mais_utilizado'] = periodo_mais_utilizado
+    respostas['Diferenca_percentual'] = diferenca_percentual
     
     # Retorna o dicionário com as análises
     return respostas
