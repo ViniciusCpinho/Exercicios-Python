5738485d5741e3cc5b1339abdf86024e38431aa5
Vinicius Pinheiro <carraravinicius155@gmail.com>
2023-04-24 22:12:00 -0400

    Prova

--- Prova/views.py
+++ Prova/views.py
@@ -11,6 +11,9 @@ FILE_PATH = os.path.join(BASE_DIR, 'Rendimentos.txt')
 
 erro = 'Não há arquivos registrados'
 
+meses = {'janeiro': 1, 'fevereiro': 2, 'março': 3, 'abril': 4, 'maio': 5, 'junho': 6, 
+         'julho': 7, 'agosto': 8, 'setembro': 9, 'outubro': 10, 'novembro': 11, 'dezembro': 12}
+
 
 def listar(request):
     try:
@@ -22,11 +25,21 @@ def listar(request):
             
             data = []
             for folha_rendimento in files_line:
-                mes, ano, dinheiro = folha_rendimento.split()
+                mes, ano, salario, despesa, saldo, investimento = folha_rendimento.split()
+                rendimento = (int(salario) * 10 / 100) * 0.1 * len(files_line)
+                if rendimento == int(saldo) * 0.1 * len(files_line):
+                    rendimento == 'Bateu a meta'
+                else: 
+                    rendimento == 'Não bateu a meta'
+                    
                 data.append({
                     'mes': mes,
                     'ano': ano,
-                    'dinheiro': dinheiro,
+                    'salario': salario,
+                    'despesa': despesa,
+                    'saldo': saldo,
+                    'investimento': investimento,
+                    'rendimento': rendimento
                 })
             return render(request, 'index_prova.html', {'dados':data})
     except FileNotFoundError:
@@ -36,15 +49,30 @@ def informar(request):
     if(request.method == 'POST'):
         mes = request.POST.get('mes', '')
         ano = request.POST.get('ano', '')
-        quantidade = request.POST.get('quantidade', '')
-        tipo = request.POST.get('tipo', '')
+        salario = request.POST.get('salario', 'Não informado')
+        despesa = request.POST.get('despesa', 'Não informado')
+        saldo = int(salario) - int(despesa)
         
-        data = f'{mes} {ano} {quantidade} {tipo}\n'
+        if(salario < despesa):
+            data = 'o salário não pode ser menor que a despesa'
+            return render(request, 'index_prova.html', {'dados':data})
         
-        with open(FILE_PATH, '+a') as file:
-            file.write(data)
-        
-        return redirect('Prova:informar_rendimento')
+        else:
+            if saldo < 0: 
+                data = 'Não foi possivel salvar'
+                return render(request, 'index_prova.html', {'dados':data})
+            else:
+                try:
+                    investimento = int(saldo) * 10/100
+                except:
+                    data = 'Não é um salário valido'
+                    return render(request, 'index_prova.html', {'dados':data})
+                data = f'{mes} {ano} {salario} {despesa} {saldo} {investimento}\n'        
+                
+                with open(FILE_PATH, '+a') as file:
+                    file.write(data)
+
+                return redirect('Prova:informar_rendimento')
     else:
         return redirect('Prova:informar_rendimento')
     
@@ -54,17 +82,21 @@ def atualizar(request):
         ano_antigo = request.POST.get('ano_antigo', '')
         mes_novo = request.POST.get('mes_novo', '')
         ano_novo = request.POST.get('ano_novo', '')
-        tipo_novo= request.POST.get('tipo_novo')
-        quantidade = request.POST.get('quantidade', '')
+        tipo= request.POST.get('tipo_novo')
+        quantidade_novo = request.POST.get('quantidade', '')
         
         with open(FILE_PATH, 'r') as file:
             lines = file.readlines()
         for i in range(len(lines)):
-                mes, ano = lines[i].split()
-                if mes == mes_antigo and ano == ano_antigo: 
-                    lines[i] = f'{mes_novo} {ano_novo} {quantidade} {tipo_novo}\n'
-                    break
-                
+            mes, ano, salario, despesa, saldo, investimento = lines[i].split()
+            if mes == mes_antigo and ano == ano_antigo: 
+                if tipo == 'despesa':
+                    lines[i] = f'{mes_novo} {ano_novo} {salario} {quantidade_novo} {investimento}\n'
+                if tipo == 'salario':
+                    lines[i] = f'{mes_novo} {ano_novo} {quantidade_novo} {despesa} {investimento}\n'
+                if tipo == 'ambos':
+                    lines[i] = f'{mes_novo} {ano_novo} {quantidade_novo} {despesa} {investimento}\n'
+                break
         with open(FILE_PATH, 'w') as file:
                 file.writelines(lines)
         
@@ -73,20 +105,19 @@ def atualizar(request):
         return redirect('Prova:informar_rendimento')
     
 def excluir(request):
-    
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         mes = request.POST.get('mes')
         ano = request.POST.get('ano')
-        tipo = request.POST.get('tipo')
     
         with open(FILE_PATH, 'r') as file:
             lines = file.readlines()
-        for i in range(len(lines)):
-                mes_arquivo, ano_arquivo, quantidade, tipo_arquivo  = lines[i].split()
-                if mes == mes_arquivo and ano == ano_arquivo and tipo == tipo_arquivo: 
-                    del lines[i]
-                    with open(FILE_PATH, 'w') as file:
-                        file.writelines(lines)
-                    break
-
-
+        new_lines = []
+        for line in lines:
+            mes_arquivo, ano_arquivo, salario, despesa, saldo, investimento = line.split()
+            if mes == mes_arquivo and ano == ano_arquivo:
+                continue
+            else:
+                new_lines.append(line)
+        with open(FILE_PATH, 'w') as file:
+            file.writelines(new_lines)
+    return redirect('Prova:informar_rendimento')
